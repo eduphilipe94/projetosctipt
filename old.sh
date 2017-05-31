d2fd491ae193051f09f91977ba12ba6ef9b0cf93
eduphilipe94 <eduardophilipe94@gmail.com>
2017-05-31 11:40:28 -0300

    Create old.sh

---
+++ old.sh
@@ -0,0 +1,13 @@
+# #!/bin/bash
+echo "Para acessar o arquivo do Banco de Dados digite seu usúario:"
+read USER
+if [ $USER = "Eduardo"]
+then
+echo " $USER, Usuário Autorizado"
+cat /var/lib/mysql/redesnassau.frm
+else
+echo "Você não tem permissão para acessar este arquivo"
+exit 127
+fi
+
+exit 0
